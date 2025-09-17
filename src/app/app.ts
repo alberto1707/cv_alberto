588cc2d98ffa137973dcb23ce39327dc3198d950
alberttho1707666 <alberttho1707666@gmail.com>
2025-09-16 22:16:16 -0400

    cv con bootstrap

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Cv } from './cv/cv';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Cv],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/cv/cv.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { TarjetaPerfil } from 'tarjeta-perfil';
+
+@Component({
+  selector: 'app-cv',
+  imports: [TarjetaPerfil],
+  templateUrl: './cv.html',
+  styleUrl: './cv.scss'
+})
+export class Cv {
+
+}
